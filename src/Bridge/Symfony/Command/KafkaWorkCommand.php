06d0015afd423b9ae814e7bd783fbd3d69dc0a04
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2025-11-10 23:49:01 +0500

    first commit

--- src/Bridge/Symfony/Command/KafkaWorkCommand.php
+++ src/Bridge/Symfony/Command/KafkaWorkCommand.php
@@ -1,19 +1,20 @@
 <?php
-declare(strict_types=1);
 
 namespace Muxtorov98\Kafka\Bridge\Symfony\Command;
 
-use Muxtorov98\Kafka\AutoDiscovery;
 use Muxtorov98\Kafka\Consumer;
 use Muxtorov98\Kafka\KafkaOptions;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class KafkaWorkCommand extends Command
+#[AsCommand(
+    name: 'kafka:work',
+    description: 'Run Kafka consumer workers'
+)]
+class KafkaWorkCommand extends Command
 {
-    protected static $defaultName = 'kafka:work';
-
     public function __construct(private KafkaOptions $options)
     {
         parent::__construct();
@@ -21,8 +22,7 @@ final class KafkaWorkCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $routing = AutoDiscovery::discover($this->options);
-        $consumer = new Consumer($this->options, $routing);
+        $consumer = new Consumer($this->options);
         return $consumer->run();
     }
 }
