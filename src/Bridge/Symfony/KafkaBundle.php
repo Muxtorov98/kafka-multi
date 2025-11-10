0eb97a899c9a4699d6f9984be684bd571fbb506f
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2025-11-10 23:36:22 +0500

    first commit

--- src/Bridge/Symfony/KafkaBundle.php
+++ src/Bridge/Symfony/KafkaBundle.php
@@ -3,11 +3,18 @@
 namespace Muxtorov98\Kafka\Bridge\Symfony;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Muxtorov98\Kafka\Bridge\Symfony\DependencyInjection\KafkaExtension;
 
 class KafkaBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
-        return new DependencyInjection\KafkaExtension();
+        return new KafkaExtension();
+    }
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
     }
 }
