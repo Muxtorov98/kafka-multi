d612942f1e211fd332282bf9029c9bd43602a7ed
Tulqin_Muxtorov98 <104984928+Muxtorov98@users.noreply.github.com>
2025-11-11 00:00:45 +0500

    first commit

--- src/Bridge/Symfony/ConfigFactory.php
+++ src/Bridge/Symfony/ConfigFactory.php
@@ -4,11 +4,19 @@ declare(strict_types=1);
 namespace Muxtorov98\Kafka\Bridge\Symfony;
 
 use Muxtorov98\Kafka\KafkaOptions;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 final class ConfigFactory
 {
-    public static function create(array $config): KafkaOptions
+    public static function create(ContainerInterface $container): KafkaOptions
     {
+        // Config Symfony parameters ichidan olinadi
+        $config = $container->getParameter('kafka');
+
+        if (!is_array($config)) {
+            throw new \RuntimeException("Kafka configuration must be an array. Given: " . gettype($config));
+        }
+
         return KafkaOptions::fromArray($config);
     }
 }
